084951bb08fd0fa3849e30eafc6335b8fa96fcbf
gulu43 <gulamnabi9th@gmail.com>
2025-01-11 11:35:01 +0530

    added admin login

--- src/Login_page.java
+++ src/Login_page.java
@@ -2,7 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 /**
  *
  * @author gulam
@@ -54,6 +58,11 @@ public class Login_page extends javax.swing.JFrame {
 
         jButton1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jButton1.setText("Submit");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -104,6 +113,44 @@ public class Login_page extends javax.swing.JFrame {
     private void passwordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passwordActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_passwordActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+         String url="jdbc:mysql://localhost:3306/library_db";
+       String mysqluser="root";
+       String mysqlpwd= "";
+       String pswrd=new String(password.getPassword());
+       String username= user_name.getText();
+       String query=("SELECT `password` FROM `admin` WHERE `user_id` ='"+username+"';");
+       try{
+           Connection conn= DriverManager.getConnection(url,mysqluser,mysqlpwd);
+           Statement stm=conn.createStatement();
+           ResultSet rs= stm.executeQuery(query);
+           if(rs.next())
+           {
+//              realpswrd means db password , we a getting from sql field name password
+                String realpswrd=rs.getString("password");
+//              checking db pass with entered password
+                if(realpswrd.equals(pswrd))
+                 {   
+                    DashBord dsh= new DashBord();
+                    dsh.setVisible(true);
+                    this.dispose();
+                 }
+                 else
+                 {
+                   JOptionPane.showMessageDialog(this,"username or password entered is wrong");  
+                 }
+           }
+           else
+               JOptionPane.showMessageDialog(this, "Wrong Username");
+              
+        }
+       catch(Exception e)
+       {   JOptionPane.showMessageDialog(this,e.getMessage()); 
+       }
+
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
