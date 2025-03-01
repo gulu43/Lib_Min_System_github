2af716550d14aba65cadc65603b78a7a51841e36
gulu43 <gulamnabi9th@gmail.com>
2025-03-01 09:11:01 +0530

    added_imgFolder_and_BackroundPanelJavaFile

---
+++ src/BackgroundPanel.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author gulam
+ */
+public class BackgroundPanel extends JPanel{
+    private Image backgroundImage;
+
+    public BackgroundPanel(String filePath) {
+        backgroundImage = new ImageIcon(filePath).getImage();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+    }
+}
